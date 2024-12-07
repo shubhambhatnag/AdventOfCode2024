61b4936563f609108a46d0d652fb937f8a05aad8
shubhambhatnag <shubhambhatnagar2017@gmail.com>
2024-12-07 00:12:32 -0500

    quickfix

--- day7/part1.py
+++ day7/part1.py
@@ -5,7 +5,7 @@ file = open("day7/input.txt","r")
 def apply(lst,target):
     if len(lst) == 1:
         return target == lst[0]
-    return apply([lst[0] + lst[1]]+lst[2:], target) or apply([lst[0] * lst[1]]+lst[2:], target) or apply([int(str(lst[0]) + str(lst[1]))]+lst[2:], target) 
+    return apply([lst[0] + lst[1]]+lst[2:], target) or apply([lst[0] * lst[1]]+lst[2:], target)
     
 total = 0
 for line in file.readlines():
--- day7/part2.py
+++ day7/part2.py
@@ -1,61 +1,27 @@
-from collections import defaultdict
+file = open("day7/input.txt","r")
 
-file = open("day6/input.txt","r")
 
-found = set()
 
-curr = ()
-area = []
-cnt = 0
+def apply(lst,target):
+    if len(lst) == 1:
+        return target == lst[0]
+    return apply([lst[0] + lst[1]]+lst[2:], target) or apply([lst[0] * lst[1]]+lst[2:], target) or apply([int(str(lst[0]) + str(lst[1]))]+lst[2:], target) 
+    
+total = 0
 for line in file.readlines():
     line = line.rstrip()
+    line = line.split(": ")
+    target = int(line[0])
+    nums = [int(i) for i in line[1].split(" ")]
 
-    area.append(list(line))
-
-    if '^' in line:
-        curr = (cnt, line.index('^'))
-
-    cnt += 1
-
-
-direction = 0
-diffs = [(-1, 0), (0, 1), (1, 0), (0, -1)] 
+    if apply(nums,target):
+        total += target
 
 
-def simulate(block):
+print(total)
 
-    found = set()
 
-    local_curr = curr
 
-    local_direction = direction
-    
-    while 0 <= local_curr[0] < len(area) and 0 <= local_curr[1] < len(area[0]):
 
-        state = (local_curr, local_direction)
 
-        if state in found:
-            return True  
-        
-        found.add(state)
-        
-  
-        next_curr = (local_curr[0] + diffs[local_direction][0], local_curr[1] + diffs[local_direction][1])
-        
 
-        if (next_curr == block) or (0 <= next_curr[0] < len(area) and 0 <= next_curr[1] < len(area[0]) and area[next_curr[0]][next_curr[1]] == '#'):
-            local_direction = (local_direction + 1) % 4
-        else:
-            local_curr = next_curr
-    
-    return False 
-
-found_loops = set()
-
-for i in range(len(area)):
-    for j in range(len(area[0])):
-        if area[i][j] != '#' and (i, j) != curr:
-            if simulate((i, j)):
-                found_loops.add((i, j))
-
-print(len(found_loops))
