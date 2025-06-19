435804883c3c458e217057af74a1c16d8ddbfc87
shubhambhatnag <shubhambhatnagar2017@gmail.com>
2025-06-18 23:34:34 -0400

    day12

--- day12/part2.py
+++ day12/part2.py
@@ -19,8 +19,8 @@ for row in range(len(garden)):
             continue
 
         area = 0
-        sides = set()
-
+        sides = {}
+        sides_fake = set()
         stack = [(row,col)]
 
         while stack != []:
@@ -35,50 +35,213 @@ for row in range(len(garden)):
 
             if curr_row != 0:
                 if garden[curr_row - 1][curr_col] != garden[curr_row][curr_col]:
-                    sides.add("above"+str(curr_row))
+                    key = "UP" + str(curr_row)
+                    if key not in sides:
+                        sides[key] = [curr]
+                    else:
+                        encountered = False
+                        for rep_row, rep_col in sides[key]:
+                            step = 1 if curr_col > rep_col else -1
+                            clear_path = True
+
+                            for i in range(rep_col + step, curr_col, step):
+                                if garden[curr_row][i] != garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                                if garden[curr_row - 1][i] == garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                               
+
+                            if clear_path:
+                                encountered = True
+                                break
+
+                        if not encountered:
+                            sides[key].append(curr)
                 else:
                     stack.append((curr_row - 1, curr_col))
             else:
-                sides.add("above"+str(curr_row))
+                key = "UP" + str(curr_row)
+                if key not in sides:
+                    sides[key] = [curr]
+                else:
+                    encountered = False
+                    for rep_row, rep_col in sides[key]:
+                        step = 1 if curr_col > rep_col else -1
+                        clear_path = True
+
+                        for i in range(rep_col + step, curr_col, step):
+                            if garden[curr_row][i] != garden[curr_row][curr_col]:
+                                clear_path = False
+                                break
+                            
+
+                        if clear_path:
+                            encountered = True
+                            break
+
+                    if not encountered:
+                        sides[key].append(curr)
 
             if curr_row != len(garden) - 1:
                 if garden[curr_row + 1][curr_col] != garden[curr_row][curr_col]:
-                    sides.add("below"+str(curr_row))
+                    key = "DOWN" + str(curr_row)
+                    if key not in sides:
+                        sides[key] = [curr]
+                    else:
+                        encountered = False
+                        for rep_row, rep_col in sides[key]:
+                            step = 1 if curr_col > rep_col else -1
+                            clear_path = True
+
+                            for i in range(rep_col + step, curr_col, step):
+                                if garden[curr_row][i] != garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                                if curr_row + 1 < len(garden) and garden[curr_row + 1][i] == garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+
+                            if clear_path:
+                                encountered = True
+                                break
+
+                        if not encountered:
+                            sides[key].append(curr)
                 else:
                     stack.append((curr_row + 1, curr_col))
             else:
-                sides.add("below"+str(curr_row))
+                key = "DOWN" + str(curr_row)
+                if key not in sides:
+                    sides[key] = [curr]
+                else:
+                    encountered = False
+                    for rep_row, rep_col in sides[key]:
+                        step = 1 if curr_col > rep_col else -1
+                        clear_path = True
+
+                        for i in range(rep_col + step, curr_col, step):
+                            if garden[curr_row][i] != garden[curr_row][curr_col]:
+                                clear_path = False
+                                break
+
+                        if clear_path:
+                            encountered = True
+                            break
+
+                    if not encountered:
+                        sides[key].append(curr)
             
             if curr_col != 0:
                 if garden[curr_row][curr_col-1] != garden[curr_row][curr_col]:
-                    sides.add("left"+str(curr_col))
+                    key = "LEFT" + str(curr_col)
+                    if key not in sides:
+                        sides[key] = [curr]
+                    else:
+                        encountered = False
+                        for rep_row, rep_col in sides[key]:
+                            step = 1 if curr_row > rep_row else -1
+                            clear_path = True
+
+                            for i in range(rep_row + step, curr_row, step):
+                                if garden[i][curr_col] != garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                                if garden[i][curr_col - 1] == garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                               
+
+                            if clear_path:
+                                encountered = True
+                                break
+
+                        if not encountered:
+                            sides[key].append(curr)
                 else:
                     stack.append((curr_row, curr_col-1))
             else:
-                sides.add("left"+str(curr_col))
+                key = "LEFT" + str(curr_col)
+                if key not in sides:
+                    sides[key] = [curr]
+                else:
+                    encountered = False
+                    for rep_row, rep_col in sides[key]:
+                        step = 1 if curr_row > rep_row else -1
+                        clear_path = True
+
+                        for i in range(rep_row + step, curr_row, step):
+                            if garden[i][curr_col] != garden[curr_row][curr_col]:
+                                clear_path = False
+                                break
+
+                        if clear_path:
+                            encountered = True
+                            break
+
+                    if not encountered:
+                        sides[key].append(curr)
 
             if curr_col != len(garden[0]) - 1:
                 if garden[curr_row ][curr_col+1] != garden[curr_row][curr_col]:
-                    sides.add("right"+str(curr_col))
+                    key = "RIGHT" + str(curr_col)
+                    if key not in sides:
+                        sides[key] = [curr]
+                    else:
+                        encountered = False
+                        for rep_row, rep_col in sides[key]:
+                            step = 1 if curr_row > rep_row else -1
+                            clear_path = True
+
+                            for i in range(rep_row + step, curr_row, step):
+                                if garden[i][curr_col] != garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                                if garden[i][curr_col + 1] == garden[curr_row][curr_col]:
+                                    clear_path = False
+                                    break
+                               
+
+                            if clear_path:
+                                encountered = True
+                                break
+
+                        if not encountered:
+                            sides[key].append(curr)
                 else:
                     stack.append((curr_row , curr_col+1))
             else:
-                sides.add("right"+str(curr_col))
-        
-        # print(area, perimeter)
+                key = "RIGHT" + str(curr_col)
+                if key not in sides:
+                    sides[key] = [curr]
+                else:
+                    encountered = False
+                    for rep_row, rep_col in sides[key]:
+                        step = 1 if curr_row > rep_row else -1
+                        clear_path = True
 
-        print(garden[row][col], sides)
-        total += area * len(sides)
+                        for i in range(rep_row + step, curr_row, step):
+                            if garden[i][curr_col] != garden[curr_row][curr_col]:
+                                clear_path = False
+                                break
+                            
+
+                        if clear_path:
+                            encountered = True
+                            break
+
+                    if not encountered:
+                        sides[key].append(curr)
+        
+  
+        sides_sum = 0
+
+        for case in sides:
+            sides_sum += len(sides[case])
+
+        total += area * sides_sum
 
 print(total)
 
             
-
-            
-
-
-
-
-
-
-
